70130e900c31abb8f1800a580715c8bd26d9d6c4
Derek Wene <djrobotfreak@gmail.com>
2016-03-07 22:39:16 -0600

    removing stupid stuff in config

--- src/main/java/com.netegreek/chattr/CoopConfiguration.java
+++ src/main/java/com.netegreek/chattr/CoopConfiguration.java
@@ -7,33 +7,8 @@ import io.dropwizard.client.JerseyClientConfiguration;
 import org.hibernate.validator.constraints.NotEmpty;
 
 public class CoopConfiguration extends Configuration {
-    @NotEmpty
-    private String template;
-
-    @NotEmpty
-    private String defaultName = "Stranger";
 
     private JerseyClientConfiguration jerseyClientConfiguration = new JerseyClientConfiguration();
-
-    @JsonProperty
-    public String getTemplate() {
-        return template;
-    }
-
-    @JsonProperty
-    public void setTemplate(String template) {
-        this.template = template;
-    }
-
-    @JsonProperty
-    public String getDefaultName() {
-        return defaultName;
-    }
-
-    @JsonProperty
-    public void setDefaultName(String defaultName) {
-        this.defaultName = defaultName;
-    }
 
     public JerseyClientConfiguration getJerseyClientConfiguration() {
         return jerseyClientConfiguration;

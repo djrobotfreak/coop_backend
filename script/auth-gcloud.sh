e16e82d8c5dde1e28675d8b484942e9a94c6bdf5
Derek Wene <djrobotfreak@gmail.com>
2016-03-24 00:14:10 -0500

    set project for gcloud

--- script/auth-gcloud.sh
+++ script/auth-gcloud.sh
@@ -2,3 +2,4 @@
 echo $GCLOUD_KEY | base64 --decode > gcloud.p12
 gcloud auth activate-service-account $GCLOUD_EMAIL --key-file gcloud.p12
 ssh-keygen -f ~/.ssh/google_compute_engine -N ""
+gcloud config set project neural-cortex-125102
